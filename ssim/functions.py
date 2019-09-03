0cb3f23485d845fbf3e4b3da4cacdc8b25a720b2
higumachan <harekumo792154@gmail.com>
2019-09-03 17:43:07 +0900

    fix bugs

--- ssim/functions.py
+++ ssim/functions.py
@@ -1,17 +1,18 @@
 import chainer.functions as F
 
 
-def create_window(window_size, channel, xp):
+def _create_window(window_size, channel, xp):
     return xp.ones((1, channel, window_size, window_size)) / (window_size ** 2)
 
 
 def ssim_loss(y, t, window_size, stride):
     n, c, w, h = y.shape
-    mu_y = F.depthwise_convolution_2d(y, create_window(window_size, c, y.xp), stride=stride)
-    mu_t = F.depthwise_convolution_2d(t, create_window(window_size, c, y.xp), stride=stride)
-    mu_y_sq = F.depthwise_convolution_2d(y * y, create_window(window_size, c, y.xp), stride=stride)
-    mu_t_sq = F.depthwise_convolution_2d(t * t, create_window(window_size, c, y.xp), stride=stride)
-    mu_ty = F.depthwise_convolution_2d(y * t, create_window(window_size, c, y.xp), stride=stride)
+    window = _create_window(window_size, c, y.xp)
+    mu_y = F.depthwise_convolution_2d(y, window, stride=stride)
+    mu_t = F.depthwise_convolution_2d(t, window, stride=stride)
+    mu_y_sq = F.depthwise_convolution_2d(y * y, window, stride=stride)
+    mu_t_sq = F.depthwise_convolution_2d(t * t, window, stride=stride)
+    mu_ty = F.depthwise_convolution_2d(y * t, window, stride=stride)
     muy_mut = (mu_y * mu_t)
 
     sq_mu_y = mu_y ** 2
@@ -35,4 +36,17 @@ if __name__ == '__main__':
     import chainer
     y = chainer.Variable(np.ones([1, 3, 3, 3]))
     t = chainer.Variable(np.ones([1, 3, 3, 3]))
-    print(ssim_loss(y, t, 3, 3))
+    assert ssim_loss(y, t, 3, 3).data == 1.0
+
+    y = chainer.Variable(np.random.random((1, 1, 3, 3)))
+    t = chainer.Variable(np.ones([1, 1, 3, 3]))
+
+    c1 = 0.01 ** 2
+    c2 = 0.03 ** 2
+    print(y.data, t.data)
+    cov = (t.data - t.data.mean()) * (y.data - y.data.mean())
+    expect = ((2 * t.data.mean() * y.data.mean() + c1) * (2 * cov + c2)) / ((y.data.mean() ** 2 + t.data.mean() ** 2 + c1) * (y.data.var() + t.data.var() + c2))
+
+    loss = ssim_loss(y, t, 3, 3).data
+    print(expect.mean(), loss)
+    assert expect.mean() == loss
